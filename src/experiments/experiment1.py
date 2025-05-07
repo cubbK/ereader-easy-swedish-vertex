5c00dd83e47961c291097a6c15856064a9728d21
Dan <yo@gmail.om>
2025-05-07 16:07:12 +0200

    add basic experiment

--- src/experiments/experiment1.py
+++ src/experiments/experiment1.py
@@ -1,8 +1,26 @@
-import dspy
+import datetime
+
 from google.cloud import aiplatform
-from src.dspy.translator import translator
-from src.dspy.metric import TranslationMetric
-from src.dspy.optimization_miprov2 import optimize_translator
 
 
 if __name__ == "__main__":
+    aiplatform.init(
+        experiment="experiment1", project="dan-ml-learn-6-ffaf", location="us-central1"
+    )
+
+    run_name = f"run-{datetime.datetime.now().strftime('%Y%m%d-%H%M%S')}"
+
+    aiplatform.start_run(run=run_name)
+
+    params = {
+        "param1": "value1",
+        "param2": "value2",
+        "param3": "value3",
+    }
+
+    aiplatform.log_params(params)
+
+    aiplatform.log_time_series_metrics({"accuracy": 0.85})
+    aiplatform.log_time_series_metrics({"accuracy": 0.90})
+
+    aiplatform.end_run()
