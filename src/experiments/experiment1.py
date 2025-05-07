8f57bada348be03026bddab391fea269ba9df4bb
Dan <yo@gmail.om>
2025-05-07 18:01:04 +0200

    run experiment and log to vertex ai experiments

--- src/experiments/experiment1.py
+++ src/experiments/experiment1.py
@@ -1,6 +1,11 @@
 import datetime
 
+import dspy
 from google.cloud import aiplatform
+
+from src.dspy.metric import TranslationMetric
+from src.dspy.optimization_miprov2 import optimize_translator, translator
+from src.dspy.evaluator import trainset
 
 
 if __name__ == "__main__":
@@ -12,15 +17,26 @@ if __name__ == "__main__":
 
     aiplatform.start_run(run=run_name)
 
-    params = {
-        "param1": "value1",
-        "param2": "value2",
-        "param3": "value3",
+    params = {"trainset": trainset, "auto": "light", "metric": TranslationMetric()}
+
+    params_log = {
+        "trainset": "trainset_object",
+        "auto": params["auto"],
+        "metric": params["metric"].__class__.__name__,
     }
 
-    aiplatform.log_params(params)
+    aiplatform.log_params(params_log)
 
-    aiplatform.log_time_series_metrics({"accuracy": 0.85})
-    aiplatform.log_time_series_metrics({"accuracy": 0.90})
+    optimized = optimize_translator(
+        trainset=params["trainset"], auto=params["auto"], metric=params["metric"]
+    )
+
+    test_example = "Folly, folly, his heart kept saying: conscious, gratuitous, suicidal folly. Of all the crimes that a Party member could commit, this one was the least possible to conceal. Actually the idea had first floated into his head in the form of a vision, of the glass paperweight mirrored by the surface of the gateleg table. As he had foreseen, Mr Charrington had made no difficulty about letting the room. He was obviously glad of the few dollars that it would bring him. Nor did he seem shocked or become offensively knowing when it was made clear that Winston wanted the room for the purpose of a love-affair. Instead he looked into the middle distance and spoke in generalities, with so delicate an air as to give the impression that he had become partly invisible. Privacy, he said, was a very valuable thing. Everyone wanted a place where they could be alone occasionally. And when they had such a place, it was only common courtesy in anyone else who knew of it to keep his knowledge to himself. He even, seeming almost to fade out of existence as he did so, added that there were two entries to the house, one of them through the back yard, which gave on an alley."
+
+    optimized_result = optimized(test_example)
+
+    score = params["metric"](dspy.Example(english=test_example), optimized_result)
+
+    aiplatform.log_time_series_metrics({"score": score})
 
     aiplatform.end_run()
